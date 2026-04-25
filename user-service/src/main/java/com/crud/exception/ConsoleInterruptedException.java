89473d84409533f6ca693e3ebe60428f7051ec06
charset-8utf <shaydetriplesix@gmail.com>
2026-04-25 16:22:55 +0300

    Добавлен конструктор ConsoleInterruptedException(String, Throwable)

--- user-service/src/main/java/com/crud/exception/ConsoleInterruptedException.java
+++ user-service/src/main/java/com/crud/exception/ConsoleInterruptedException.java
@@ -7,4 +7,14 @@ public class ConsoleInterruptedException extends UserServiceException {
     public ConsoleInterruptedException(String message) {
         super(message);
     }
+
+    /**
+     * Создаёт исключение с сообщением и причиной.
+     *
+     * @param message детальное описание ошибки
+     * @param cause   исходная причина
+     */
+    public ConsoleInterruptedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
