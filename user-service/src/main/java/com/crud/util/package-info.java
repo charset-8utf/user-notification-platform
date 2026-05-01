1dfc58bb313b6ce3ef77cac85b2d5b627aa2eef0
charset-8utf <shaydetriplesix@gmail.com>
2026-05-01 03:33:13 +0300

    Удалены устаревшие util-классы, обновлены ресурсы

--- user-service/src/main/java/com/crud/util/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Вспомогательные утилиты.
- * <p>
- * {@link com.crud.util.HibernateUtil} – синглтон для управления SessionFactory,
- * регистрирует shutdown hook для корректного завершения.
- * </p>
- */
-package com.crud.util;
