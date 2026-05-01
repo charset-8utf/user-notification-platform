54c8b7e32bf237493a0db4d996729ce15da8e596
charset-8utf <shaydetriplesix@gmail.com>
2026-05-01 03:32:49 +0300

    Обновлены исключения - добавлены новые типы

--- user-service/src/main/java/com/crud/exception/ConsoleInterruptedException.java
+++ user-service/src/main/java/com/crud/exception/ConsoleInterruptedException.java
@@ -1,19 +1,14 @@
 package com.crud.exception;
 
 /**
- * Исключение, возникающее при прерывании потока ввода из консоли.
+ * Прерывание консольного ввода.
  */
 public class ConsoleInterruptedException extends UserServiceException {
+
     public ConsoleInterruptedException(String message) {
         super(message);
     }
 
-    /**
-     * Создаёт исключение с сообщением и причиной.
-     *
-     * @param message детальное описание ошибки
-     * @param cause   исходная причина
-     */
     public ConsoleInterruptedException(String message, Throwable cause) {
         super(message, cause);
     }
--- user-service/src/main/java/com/crud/exception/package-info.java
+++ user-service/src/main/java/com/crud/exception/package-info.java
@@ -1,13 +1,4 @@
 /**
- * Иерархия кастомных исключений.
- * <p>
- * Базовое {@link com.crud.exception.UserServiceException} и его наследники:
- * {@link com.crud.exception.UserNotFoundException},
- * {@link com.crud.exception.UserAlreadyExistsException},
- * {@link com.crud.exception.DataAccessException},
- * {@link com.crud.exception.ValidationException},
- * {@link com.crud.exception.ConsoleInterruptedException}.
- * Обеспечивают понятную обработку ошибок.
- * </p>
+ * Кастомные исключения приложения.
  */
 package com.crud.exception;
