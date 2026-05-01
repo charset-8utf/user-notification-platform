20adb129ad1622786168152980f0ba29c51ad2f5
charset-8utf <shaydetriplesix@gmail.com>
2026-05-01 03:32:48 +0300

    Обновлена сущность User - добавлены связи

--- user-service/src/main/java/com/crud/entity/package-info.java
+++ user-service/src/main/java/com/crud/entity/package-info.java
@@ -1,8 +1,4 @@
 /**
  * JPA-сущности.
- * <p>
- * Содержит класс {@link com.crud.entity.User}, отображаемый на таблицу {@code users} в PostgreSQL.
- * Использует аннотации Jakarta Persistence и паттерн Builder для удобного создания объектов.
- * </p>
  */
 package com.crud.entity;
