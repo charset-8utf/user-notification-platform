7bf410a20c3953be1321894e4bcda901fa50e2ca
charset-8utf <shaydetriplesix@gmail.com>
2026-06-09 21:57:44 +0300

    Исправлен compensation E2E: DLT listener и публикация в Kafka. - Отдельная dltKafkaListenerContainerFactory без повторного DLT-retry - E2E: публикация JSON в user-notifications.DLT без parse.key

--- notification-service/src/main/java/com/notification/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/com/notification/config/KafkaConsumerConfig.java
@@ -46,6 +46,20 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
+    public ConsumerFactory<String, NotificationEmailRequest> dltConsumerFactory(
+            KafkaProperties properties, JsonMapper jsonMapper
+    ) {
+        Map<String, Object> config = properties.buildConsumerProperties();
+        JacksonJsonDeserializer<NotificationEmailRequest> jsonDeserializer =
+                new JacksonJsonDeserializer<>(NotificationEmailRequest.class, jsonMapper, false);
+        return new DefaultKafkaConsumerFactory<>(
+                config,
+                new StringDeserializer(),
+                jsonDeserializer
+        );
+    }
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, NotificationEmailRequest> kafkaListenerContainerFactory(
             ConsumerFactory<String, NotificationEmailRequest> consumerFactory,
             DefaultErrorHandler kafkaErrorHandler,
@@ -57,6 +71,20 @@ public class KafkaConsumerConfig {
         factory.setConcurrency(concurrency);
         factory.setCommonErrorHandler(kafkaErrorHandler);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        return factory;
+    }
+
+    /**
+     * DLT-топик: без повторного DLT-retry и без manual ack (сообщения из kafka-console-producer в E2E).
+     */
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, NotificationEmailRequest> dltKafkaListenerContainerFactory(
+            ConsumerFactory<String, NotificationEmailRequest> dltConsumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, NotificationEmailRequest> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(dltConsumerFactory);
+        factory.setConcurrency(1);
         return factory;
     }
 
